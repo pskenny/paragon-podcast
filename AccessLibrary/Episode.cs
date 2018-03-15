2a26aa491c07f1cfe6bf1100f3fbb255920381f7
Paul Kenny <paul.s.kenny@gmail.com>
2018-03-15 11:53:25 +0000

    Move Episode, Channel classes

--- AccessLibrary/Episode.cs
+++ AccessLibrary/Episode.cs
@@ -1,27 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace AccessLibrary
 {
-    class Episode
+    public class Episode : INotifyPropertyChanged
     {
-        public int Id { get; set; }
-        public int ChannelId { get; set; }
-        public string Title { get; set; }
-        public string Link { get; set; }
-        public string Guid { get; set; }
-        public string Description { get; set; }
-        public string EnclosureUrl { get; set; }
-        public int EnclosureLength { get; set; }
-        public string EnclosureType { get; set; }
-        public string Category { get; set; }
-        public string PublishDate { get; set; }
-        public string ItunesAuthor { get; set; }
-        public string ItunesExplicit { get; set; }
-        public string ItuneSubtitles { get; set; }
-        public string ItunesSummary { get; set; }
-        public string ItunesDuration { get; set; }
-        public string Keywords { get; set; }
+        public int channelId { get; set; }
+        public String title { get; set; }           //title information
+        public String link { get; set; }            //URL to main podcast website
+        public String guid { get; set; }            //optional tag for audio file location
+        public String description { get; set; }     //detailed description of podcast show
+        public String enclosureUrl { get; set; }    //audio file location
+        public int enclosureLength { get; set; }    //audio file length in bytes
+        public String enclosureType { get; set; }   //audio file type e.g audio/mpeg
+        public String category { get; set; }        //podcast category
+        public DateTime pubDate { get; set; }       //date when podcast RSS was published, see here for formatting
+        public String itunesAuthor { get; set; }    //Author information
+        public String itunesExplicit { get; set; }  //Whether podcast episode has explicit content e.g Yes/No
+        public String itunesSubtitle { get; set; }  //description of podcast show
+        public String itunesSummary { get; set; }   //description of podcast show
+        public String itunesDuration { get; set; }  //length of podcast episode in HH:MM:SS formatting
+        public String keywords { get; set; }        //keywords associated with podcast content
+        public String fileLocation { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
