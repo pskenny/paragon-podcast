387acd2f37c251e0ee773061d13128eb70779295
Paul Kenny <paul.s.kenny@gmail.com>
2018-04-16 21:33:03 +0100

    Remove iTunes namespace data

--- AccessLibrary/Episode.cs
+++ AccessLibrary/Episode.cs
@@ -20,13 +20,7 @@ namespace AccessLibrary
         public String EnclosureType { get; set; }   //audio file type e.g audio/mpeg
         public String Category { get; set; }        //podcast category
         public DateTime PubDate { get; set; }       //date when podcast RSS was published, see here for formatting
-        public String ItunesAuthor { get; set; }    //Author information
-        public String ItunesExplicit { get; set; }  //Whether podcast episode has explicit content e.g Yes/No
-        public String ItunesSubtitle { get; set; }  //description of podcast show
-        public String ItunesSummary { get; set; }   //description of podcast show
-        public String ItunesDuration { get; set; }  //length of podcast episode in HH:MM:SS formatting
         public String Keywords { get; set; }        //keywords associated with podcast content
-        public String FileLocation { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
     }
